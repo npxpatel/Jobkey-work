c89de756cf75335ac549de67b57e4ff938833322
npxpatel <nirajkumarpatel21@gmail.com>
2024-12-22 12:11:15 +0530

    Fixed CORS

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT;
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+}));
+
 app.use(json());
 
 app.use('/api/users', userRoutes)
